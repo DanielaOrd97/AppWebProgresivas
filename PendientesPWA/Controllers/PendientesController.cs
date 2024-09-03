e2f27f642ef7a95690959a36bff4bbcdc3623405
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-09-02 20:22:53 -0600

    Metodos Get y Put

--- PendientesPWA/Controllers/PendientesController.cs
+++ PendientesPWA/Controllers/PendientesController.cs
@@ -1,23 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using PendientesPWA.Models;
 using PendientesPWA.Models.DTOs;
+using PendientesPWA.Services;
 
 namespace PendientesPWA.Controllers
 {
+
+    //PREGUNTA DE ENTREVISTA: PRINCIPIOS SOLID
+
     [Route("api/[controller]")]
     [ApiController]
     public class PendientesController : ControllerBase
     {
-        public PendientesController(PendientesContext context)
+        public PendientesController(PendientesContext context, IHubContext<PendientesHub> hubContext)
         {
             Context = context;
+            HubContext = hubContext;
         }
 
         public PendientesContext Context { get; }
+        public IHubContext<PendientesHub> HubContext { get; }
 
         [HttpPost]
-        public IActionResult Post(PendienteDTO dto)
+        public async Task<IActionResult> Post(PendienteDTO dto)
         {
             if (string.IsNullOrWhiteSpace(dto.Descripcion))
             {
@@ -35,7 +42,54 @@ namespace PendientesPWA.Controllers
             Context.SaveChanges();
 
             //Informar con SignarR.
+            await HubContext.Clients.All.SendAsync("NuevoPendiente", p); //se envia a todos incluido yo debido a la base de datos local y la que esta en linea. Por el autoincrementable, deben estar iguales los datos.
 
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datos = Context.Pendiente;
+            return Ok(datos.Select(x => new
+            {
+                x.Id,
+                x.Descripcion,
+                x.Estado
+            }));
+        }
+
+        [HttpPut("editarDescripcion")]
+        public async Task<IActionResult> EditarDescripcion(PendienteDTO dto)
+        {
+            var pendiente = Context.Pendiente.Find(dto.Id);
+
+            if(pendiente == null)
+            {
+                return NotFound();
+            }
+
+            pendiente.Descripcion = dto.Descripcion;
+            Context.Update(pendiente);
+            int total = Context.SaveChanges(); //Porque EntityFramework se asegura que el dato anterior y el nuevo no se repitan. Si se repiten, no hay cambios.
+
+            //Notificar con signalr en caso de haber cambios.
+            if(total > 0)
+            {
+                await HubContext.Clients.All.SendAsync("PendienteEditado", new
+                {
+                    pendiente.Id,
+                    pendiente.Descripcion
+                });
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("editarEstado")]
+        public IActionResult EditarEstado(PendienteDTO dto)
+        {
             return Ok();
         }
     }
--- PendientesPWA/Services/PendientesHub.cs
+++ PendientesPWA/Services/PendientesHub.cs
@@ -4,6 +4,6 @@ namespace PendientesPWA.Services
 {
     public class PendientesHub:Hub
     {
-
+        
     }
 }
