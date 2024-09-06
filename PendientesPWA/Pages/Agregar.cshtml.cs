0d4d27a9fe79346f16f2bdeb93770e67d37abdb5
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-09-06 10:02:52 -0600

    Vista de lista con menu flotantes para cada elemento

---
+++ PendientesPWA/Pages/Agregar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PendientesPWA.Pages
+{
+    public class AgregarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PendientesPWA/wwwroot/Main.js
@@ -0,0 +1,13 @@
+﻿let menu = document.querySelector(".flotante");
+let main = document.querySelector("main");
+
+if (menu) {
+    document.addEventListener("click", function (e) {
+        if (e.target.tagName == "TD" && e.target == e.target.parentElement.lastElementChild) {
+            e.target.append(menu);
+        }
+       else  if (menu.parentElement != main && e.target.parentElement != menu) {
+            main.append(menu);
+        }
+    });
+}
