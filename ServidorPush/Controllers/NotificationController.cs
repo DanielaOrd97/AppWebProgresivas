ad6d00fbce652e3896a895b5eaecb2c2f5439b1d
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-11-11 17:53:40 -0600

    Notificaciones Push

---
+++ ServidorPush/Controllers/NotificationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServidorPush.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Suscribir()
+        {
+            return Ok();
+        }
+
+        [HttpGet("/enviar/{mensaje}")]
+        public IActionResult EnviarNotificacion(string mensaje)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ServidorPush/Services/NotificationService.cs
@@ -0,0 +1,26 @@
+﻿using Org.BouncyCastle.Asn1.Mozilla;
+using WebPush;
+
+namespace ServidorPush.Services
+{
+    public class NotificationService
+    {
+        WebPushClient pushClient = new();
+        VapidDetails vapid;
+
+        public NotificationService(IConfiguration configuration)
+        {
+            vapid = new()
+            {
+                Subject = configuration["Vapid:subject"],
+                PrivateKey = configuration["Vapid:privateKey"],
+                PublicKey = configuration["Vapid:publicKey"]
+            };
+        }
+
+        public async void EnviarNotificacion(PushSubscription cliente, string mensaje)
+        {
+            await pushClient.SendNotificationAsync(cliente, mensaje, vapid);
+        }
+    }
+}
